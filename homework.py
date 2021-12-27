f2338f272b74d43af547b2088b4cc937bd4d93fd
Татьяна Марковская <markovsk33@gmail.com>
2021-12-27 20:30:44 +0300

    try2

--- homework.py
+++ homework.py
@@ -1,19 +1,6 @@
 class InfoMessage:
-    """Информационное сообщение о тренировке."""
-    def init(self, training_type: str, 
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float,
-                 ) -> None:
-                 self.training_type = training_type
-                 self.duration = duration
-                 self.distance = distance
-                 self.speed = speed
-                 self.calories = calories               
-
-    def __init__(self, 
-                 training_type: str, 
+    """Информационное сообщение о тренировке."""              
+    def __init__(self, training_type: str,
                  duration: float,
                  distance: float,
                  speed: float,
