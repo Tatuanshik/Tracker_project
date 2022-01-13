900fdb8a63e32e19c2b02e83f1365a800996e602
Tatyna <markovsk33@gmail.com>
2022-01-13 12:12:47 +0300

    fgf

--- homework.py
+++ homework.py
@@ -17,7 +17,7 @@ class InfoMessage:
                 'Дистанция: {distance:.3f} км; '
                 'Ср. скорость: {speed:.3f} км/ч; '
                 'Потрачено ккал: {calories:.3f}.'
-                )
+    )
 
     def get_message(self) -> str:
         return self.MESSAGE.format(**asdict(self))
@@ -116,8 +116,8 @@ class Swimming(Training):
         return value_pool / self.M_IN_KM / self.duration
 
     def get_spent_calories(self) -> float:
-        return((self.get_mean_speed() + self.COEFF_1) 
-                * self.COEFF_2 * self.weight)
+        return((self.get_mean_speed() + self.COEFF_1)
+              * self.COEFF_2 * self.weight)
 
 
 def read_package(workout_type: str, data: list) -> Training:
